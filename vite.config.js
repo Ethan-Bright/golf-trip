c8f8c4795d91c10ec7ed32d8d6099a13231796de
Ethan-Bright <brightethan14@gmail.com>
2025-10-21 15:57:42 +0200

    Refactor Vite configuration to simplify base path handling

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'build' ? '/golf-trip/' : '/',
-}))
+  base: '/', 
+})
+
